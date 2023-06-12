7d3858f73442f02994acb8ef49e13b497aca8c79
skamma <surojbct066@gmail.com>
2023-06-12 16:17:01 +0545

    working

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,7 +1,26 @@
+const mybtn=this.document.getElementById('myTopBtn');
+// window.onscroll=function()
+// {
+//   if(this.document.body.scrollTop>20||this.document.documentElement.scrollTop>20){
+//     mybtn.style.display="block";
+//   }else{
+//     mybtn.style.display="none";
+//   }
+// }
 window.addEventListener('scroll',function(){
   const header = document.querySelector('header');
-  header.classList.toggle("sticky", window.scrollY > 0);
+  header.classList.toggle("sticky", window.scrollY > 20);
+  if(this.document.body.scrollTop>20||this.document.documentElement.scrollTop>20){
+    mybtn.style.display="block";
+  }else{
+    mybtn.style.display="none";
+  }
 });
+
+function gotoTop(){
+document.body.scrollTop=0;
+document.documentElement.scrollTop=0;
+}
 
 function togglemenu(){
   const menutoggle = document.querySelector('.menutoggle');
